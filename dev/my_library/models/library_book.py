cddf386cb8873a0d20a474e24d844c1297dce881
Chuc Nguyen <nvchuc.work@gmail.com>
2025-03-04 01:10:51 +0700

    add funtion insert book image and book file

--- dev/my_library/models/library_book.py
+++ dev/my_library/models/library_book.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 
 class LibraryBook(models.Model):
     _name = 'library.book'
@@ -8,6 +8,7 @@ class LibraryBook(models.Model):
     author = fields.Char(string = 'Tác giả')
     publish_date = fields.Date(string = 'Ngày xuất bản')
     isbn = fields.Char(string = 'ISBN')
+    count = fields.Char(string='Số lượng')
     category = fields.Selection(
         selection=[
             ('novel', 'Tiểu thuyết'),
@@ -18,8 +19,22 @@ class LibraryBook(models.Model):
         string='Thể loại',
         default='novel',
     )
+    book_file = fields.Binary(string='File sách', attachment=True)
+    cover_image = fields.Binary(string='Ảnh bìa', attachment=True)
 
     @api.depends('name', 'author')
     def _compute_display_name(self):
         for book in self:
             book.display_name = f"{book.name} - {book.author or 'Không rõ'}"
+
+    @api.constrains('book_file', 'book_filename')
+    def _check_book_file(self):
+        for record in self:
+            if record.book_file and not record.book_filename.lower().endswith('.pdf'):
+                raise exceptions.ValidationError("Chỉ chấp nhận file PDF cho sách")
+
+    @api.constrains('cover_image')
+    def _check_cover_image(self):
+        for record in self:
+            if record.cover_image and not any(record.cover_image.startswith(prefix) for prefix in [b'\xFF\xD8', b'\x89PNG']):
+                raise exceptions.ValidationError("Chỉ chấp nhận ảnh JPG hoặc PNG cho bìa sách")
